2a15bf36531393a56d6df9682be1c5b42022cc56
Nhatminhkakaka <nhatminhnguyenhoang43@gmail.com>
2026-02-16 12:52:51 +0700

    fix supabase client for ssr

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,10 @@
 import { createBrowserClient } from "@supabase/ssr";
 
 export function createSupabaseClient() {
+  if (typeof window === "undefined") {
+    return null as any;
+  }
+
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
